7ed36a1bf669d7b43bce21c6a49b2f503da976a5
hwolf0610 <hwolf0610@outlook.com>
2019-12-28 13:23:53 +0800

    Create_my_Nuxt JS_Tutorial_Step By Step__1

---
+++ components/Article.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: "Article.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/Articles.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Articles</h1>
+
+       <div v-for="article in articles" >
+            <h2>{{article.title}}</h2>
+            <h3>by {{article.author}}</h3>
+            <br/>
+            
+        </div>
+    </div>
+
+    
+</template>
+
+<script>
+import articles from '~/data/articles.json'
+import acticle from '~/components/article';
+
+export default {
+    name: "Articles.vue",
+    data:function(){
+        return{
+            articles: articles
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
